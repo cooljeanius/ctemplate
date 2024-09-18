626467dbf9427ddbddf8997460b50c1715c5a122
Eric Gallager <egall@gwmail.gwu.edu>
2024-09-17 20:57:48 -0400

    a bit more minor tweaking

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Before using, you should figure out all the .m4 macros that your
-# configure.m4 script needs and make sure they exist in the m4/
+# configure.ac script needs and make sure they exist in the m4/
 # directory.
 #
 # These are the files that this script might edit:
@@ -13,10 +13,10 @@
 
 # Because libtoolize isn't in the hermetic build, autogen doesn't run it.
 # However, it should be run manually periodically to update these files:
-#    in .: ltmain.sh
+#    in build-aux: ltmain.sh
 #    in m4: libtool.m4 ltoptions.m4 ltsugar.m4 ltversion.m4 lt~obsolete.m4
 
-echo "It would probably be better to just run \`autoreconf' with your favorite flags, but whatever..."
+echo "It would probably be better to just run 'autoreconf' with your favorite flags, but whatever..."
 set -ex
 rm -rf autom4te.cache
 
@@ -25,5 +25,6 @@ test -n "`which autoconf`" && test -x "`which autoconf`" && autoconf -f -W all,n
 test -n "`which autoheader`" && test -x "`which autoheader`" && autoheader -f -W all
 test -n "`which automake`" && test -x "`which automake`" && automake -a -c -f -W all
 
+if test -r configure~ && test -w . && test -d .git; then rm -f configure~; fi
 rm -rf autom4te.cache
 exit 0
